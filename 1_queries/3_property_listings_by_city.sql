dbabfb81d8ed5094cd9469bc3d753e1077ece233
Alexander Nguyen <cernvii@gmail.com>
2021-02-05 02:14:58 +0000

    getAllProperties updated

--- 1_queries/3_property_listings_by_city.sql
+++ 1_queries/3_property_listings_by_city.sql
@@ -6,3 +6,10 @@ GROUP by properties.id
 HAVING AVG(property_reviews.rating) >= 4
 ORDER BY cost_per_night
 LIMIT 10;
+
+SELECT properties.*, avg(property_reviews.rating) as average_rating
+FROM properties
+JOIN property_reviews ON properties.id = property_id
+WHERE CITY LIKE '%ancouv%'
+GROUP BY properties.id
+ORDER BY cost_per_night;
